64084dfdca20408562ffba49fd366abb8b6974fe
ChesterFL <Chester@favorlabs.io>
2022-07-12 16:56:09 +0800

    feat: different network ids correspond to different faucets

--- src/config/url.ts
+++ src/config/url.ts
@@ -2,3 +2,12 @@ export const defaultApi: string = 'http://localhost:1633';
 export const sessionStorageApi: string = 'api_host';
 export const time = 3e4; // refresh time
 export const speedTime = 5000;
+
+export const faucet: Record<number, string> = {
+  19: 'https://faucet.polygon.technology/',
+  20: 'https://faucet.metissafe.tech/',
+};
+
+export const getFaucet = (network_id: number): string => {
+  return faucet[network_id] || faucet[19];
+};
