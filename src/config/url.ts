ce81706ccb5e5749ddfd74a38a74d5f971ac3ea3
ChesterFL <Chester@favorlabs.io>
2022-07-25 15:20:54 +0800

    misc: change url

--- src/config/url.ts
+++ src/config/url.ts
@@ -1,4 +1,4 @@
-export const defaultApi: string = 'https://test-node.favorlabs.io:1603';
+export const defaultApi: string = 'https://polygon.testnet.favorlabs.io:1603';
 export const sessionStorageApi: string = 'api_host';
 export const time = 3e4; // refresh time
 export const speedTime = 5000;
